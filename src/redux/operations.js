d87615557e12495d54b523e9406f0de8e670dcc3
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-25 18:21:59 +0300

    refactoring contactSlice

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,3 +1,13 @@
 import axios from 'axios';
 import { createAsyncThunk } from "@reduxjs/toolkit"
 const BASE_URL = 'https://653927fee3b530c8d9e80563.mockapi.io'
+
+export const fetchContacts = {
+
+}
+export const addContact = {
+
+}
+export const deleteContact = {
+
+}
