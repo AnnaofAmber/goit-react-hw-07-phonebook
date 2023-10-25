2cb2fc39f8dc34e451937834b22090f49297d265
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-25 18:30:10 +0300

    creating operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,12 +2,36 @@ import axios from 'axios';
 import { createAsyncThunk } from "@reduxjs/toolkit"
 const BASE_URL = 'https://653927fee3b530c8d9e80563.mockapi.io'
 
-export const fetchContacts = {
-
-}
-export const addContact = {
-
-}
-export const deleteContact = {
-
-}
+export const fetchContacts = createAsyncThunk(
+    "contacts/fetchAll", async(_, thunkAPI) => {
+        try{
+            const response = await axios.get("/contacts")
+            return response.data
+        }
+        catch(error){
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
+export const addContact = createAsyncThunk(
+    "contacts/addContact", async(name, thunkAPI) => {
+        try{
+            const response = await axios.post("/contacts", {name})
+            return response.data
+        }
+        catch(error){
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
+export const deleteContact = createAsyncThunk(
+    "contacts/deleteContact", async(contactId, thunkAPI) => {
+        try{
+            const response = await axios.post(`/contacts/${contactId}`)
+            return response.data
+        }
+        catch(error){
+            return thunkAPI.rejectWithValue(error.message)
+        }
+    }
+)
